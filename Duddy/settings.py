34f7f34a90b05bfd95ea79ab0c9ad9d39f7ddcd0
Grisbee <pawel.sliwkaa1@gmail.com>
2024-12-03 22:58:25 +0100

    .

--- Duddy/settings.py
+++ Duddy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fb7dg3cl6=v=*r0y%%!34#r(+x3rm#yb@s6*6y1qkcyl=!@(5%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
